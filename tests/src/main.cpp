207da1fecd2cfd37df7d164116f2149a6f2c9176
chebykinn <ivan@chebykinn.ru>
2016-09-28 21:58:09 +0300

    Removed SampleGraph

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,33 +1,4 @@
-#include <cstddef>
-#include <iostream>
-
 #include <gtest/gtest.h>
-
-#include <libgraph/IVertexData.h>
-#include <libgraph/IEdge.h>
-#include <libgraph/IConnector.h>
-#include <libgraph/IGraph.h>
-#include <libgraph/SampleGraph.h>
-using namespace libgraph;
-
-class TestGraph : public ::testing::Test{
-protected:
-	SampleGraph *graph;
-	void SetUp() {
-		graph = new SampleGraph();
-	}
-	void TearDown(){
-		delete graph;
-	}
-};
-
-TEST_F(TestGraph, test1){
-	ASSERT_EQ(graph->getVertexCount(), 0);
-}
-
-TEST_F(TestGraph, test2){
-	ASSERT_TRUE(graph->createVertex(NULL) == 0);
-}
 
 int main(int argc, char *argv[]){
 	::testing::InitGoogleTest(&argc, argv);
