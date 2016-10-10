91420dac6dc24a163f2706a666ee92ca8408283f
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2016-10-11 00:29:37 +0300

    Changed IGraph interface

---
+++ libgraph/src/include/libgraph/IVertexStorage.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <libgraph/private/windll.h>
+#include <libgraph/private/constants.h>
+
+namespace libgraph {
+	template <typename _VertexValue>
+	class IVertexStorage {
+	public:
+		virtual vertex_id_t getVertexCount() = 0;
+		virtual vertex_id_t createVertex(_VertexValue) = 0;
+		virtual bool containsVertex(vertex_id_t id) = 0;
+		virtual void removeVertex(vertex_id_t id) = 0;
+		virtual void clearVertices() = 0;
+		virtual ~IVertexStorage() { };
+	};
+}
+#pragma once
