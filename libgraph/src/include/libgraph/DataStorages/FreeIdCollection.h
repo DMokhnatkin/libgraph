e926fbe4ebe25937a2c61ce551c352d90a9e186d
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2016-10-12 20:41:36 +0300

    Small fixes in freeIdColelction

--- libgraph/src/include/libgraph/DataStorages/FreeIdCollection.h
+++ libgraph/src/include/libgraph/DataStorages/FreeIdCollection.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <unordered_set>
 #include <vector>
@@ -13,14 +13,68 @@ namespace libgraph {
 	private:
 		std::vector<T> data;
 		std::unordered_set<unsigned int> freeId;
-		int ct = 0;
+		unsigned int ct = 0;
 	public:
 		FreeIdCollection();
+
+		/**
+		* \brief Get elements count.
+		* 
+		* Θ(1) time complexity.
+		* \return Number of elements in collection.
+		*/
 		virtual unsigned int getCount() override;
-		virtual unsigned int addData(T) override;
-		virtual bool contains(unsigned int);
-		virtual T getData(unsigned int);
-		virtual bool removeData(unsigned int) override;
+
+		/**
+		 * \brief Get max id in collection.
+		 * 
+		 * Max id can be currently used id or free id. 
+		 * So data with this id сan not exist in collection now (but it was exist some time before).
+		 * Θ(1) time complexity.
+		 * \return max ever existed in collection id.
+		 */
+		virtual unsigned int getMaxId();
+
+		/**
+		* \brief Create element with given data.
+		* 
+		* \param data Data to store in element.
+		* \return ID of given element.
+		*/
+		virtual unsigned int addData(T data) override;
+
+		/**
+		* \brief Check if given id is in collection.
+		* 
+		* \param id element id to check.
+		* \return true if exists, false if not.
+		*/
+		virtual bool contains(unsigned int id);
+
+		/**
+		* \brief Get data from given element id.
+		* 
+		* \param id Element id.
+		* \throw std::invalid_argument If key is not in collection.
+		* \return Data if key exists in collection.
+		*/
+		virtual T getData(unsigned int id);
+
+		/**
+		* \brief Delete element from collection by id.
+		* 
+		* Delete element from collection by id. By default,
+		* element will be removed from collection, and it's id will be pushed to free ids.
+		* (so collection size will not be reduced).
+		* \param v Id of element to delete.
+		* \return False if element is not in collection, true if deletion is successful.
+		*/
+		virtual bool removeData(unsigned int v) override;
+
+		/**
+		* \brief Clear collection.
+		* 
+		*/
 		virtual void clear();
 		virtual ~FreeIdCollection() { };
 	};
--- libgraph/src/include/libgraph/DataStorages/FreeIdCollection.tpp
+++ libgraph/src/include/libgraph/DataStorages/FreeIdCollection.tpp
@@ -1,4 +1,4 @@
-#include <stdexcept>
+﻿#include <stdexcept>
 #include <libgraph/DataStorages/FreeIdCollection.h>
 
 using namespace libgraph;
@@ -8,30 +8,21 @@ FreeIdCollection<T>::FreeIdCollection() {
 
 }
 
-/**
-* \brief Check if given id is in collection.
-* \param id element id to check.
-* \return true if exists, false if not.
-*/
 template <typename T>
 bool FreeIdCollection<T>::contains(unsigned int id) {
 	return id < data.size() && freeId.count(id) == 0;
 }
 
-/**
-* \brief Get elements count.
-* \return Number of elements in collection.
-*/
 template <typename T>
 unsigned int FreeIdCollection<T>::getCount() {
 	return ct;
 }
 
-/**
-* \brief Create element with given data.
-* \param data Data to store in element.
-* \return ID of given element.
-*/
+template <typename T>
+unsigned FreeIdCollection<T>::getMaxId() {
+	return data.size();
+}
+
 template <typename T>
 unsigned int FreeIdCollection<T>::addData(T data) {
 	vertex_id_t id;
@@ -47,12 +38,6 @@ unsigned int FreeIdCollection<T>::addData(T data) {
 	return id;
 }
 
-/**
-* \brief Get data from given element id.
-* \param id Element id.
-* \throw std::invalid_argument If key in not in collection.
-* \return Data if key exists in collection.
-*/
 template <typename T>
 T FreeIdCollection<T>::getData(unsigned int id) {
 	if (!contains(id))
@@ -60,30 +45,17 @@ T FreeIdCollection<T>::getData(unsigned int id) {
 	return data[id];
 }
 
-/**
-* \brief Delete element from collection by id.
-* \param v Id of element to delete.
-* \return False if element is not in collection, true if deletion is successful.
-*/
 template <typename T>
 bool FreeIdCollection<T>::removeData(unsigned int v) {
 	// if v is not in collection, return false
 	if (!contains(v))
 		return false;
-	if (v == data.size() - 1)
-		// if v is last in vector, reduce vector size
-		data.pop_back();
-	else {
-		// add v to free id collection
-		freeId.insert(v);
-	}
+	// add v to free id collection
+	freeId.insert(v);
 	ct--;
 	return true;
 }
 
-/**
-* \brief Clear collection.
-*/
 template <typename T>
 void FreeIdCollection<T>::clear() {
 	data.clear();
